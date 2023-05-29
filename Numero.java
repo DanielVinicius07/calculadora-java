d05dcc0cef2a7b1068e6644188590bfe258710a3
Daniel Vinicius <danielvinicius77dv7@gmail.com>
2023-05-29 00:35:43 -0300

    commit initial

---
+++ Numero.java
@@ -0,0 +1,10 @@
+public class Numero {
+    private int valor;
+        this.valor=valor;
+}
+public void setValor(int valor){
+    this.valor=valor;
+}
+public int getValor(){
+    return this.valor;
+}
